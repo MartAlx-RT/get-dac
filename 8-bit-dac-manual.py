74f0829a717e81fb9cc98a5d0b6fea3df3164a9a
MartAlx-RT <martynovalexvit99@gmail.com>
2026-02-17 15:55:16 +0300

    script

---
+++ 8-bit-dac-manual.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+#import time
+GPIO.setmode(GPIO.BCM)
+
+dynamic_range = 3.3
+pinout = [22, 27, 17, 26, 25, 21, 20, 16]
+GPIO.setup(pinout, GPIO.OUT)
+
+def voltage_to_number(voltage):
+    if(not(0.0 <= voltage <= dynamic_range)):
+        print(f"Voltage is out of range (0.0 ... {dynamic_range:.2f} V)")
+        print("Set zero.")
+        return 0
+    return int(voltage / dynamic_range * 255)
+
+def number_to_dac(number, pinout):
+    GPIO.output(pinout, [int(element) for element in bin(number)[2:].zfill(8)])
+
+voltage = 0
+number = 0
+try:
+    while True:
+        try:
+            voltage = float(input("Enter voltage (V): "))
+            number = voltage_to_number(voltage)
+            number_to_dac(number, pinout)
+        
+        except ValueError:
+            print("This is NAN, try again")
+finally:
+    GPIO.output(pinout, 0)
+    GPIO.cleanup()

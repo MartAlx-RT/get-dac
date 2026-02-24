c9c87f26d0856e6a38e3410980caaaedc5692711
MartAlx-RT <mav07@list.ru>
2026-02-24 13:43:37 +0300

    i2c

---
+++ mcp4725_driver.py
@@ -0,0 +1,37 @@
+import smbus
+
+class MCP4725:
+    def __init__(self, dynamic_range, address=0x61, verbose = True):
+        self.bus = smbus.SMBus(1)
+    
+        self.address = address
+        self.wm = 0x00
+        self.pds = 0x00
+    
+        self.verbose = verbose
+        self.dynamic_range = dynamic_range
+
+    def deinit(self):
+        self.bus.close()
+
+    def set_number(self, number):
+        if not isinstance(number, int):
+            print("На вход ЦАП можно подавать только целые числа")
+    
+        if not (0 <= number <= 4095):
+            print("Число выходит за разраядность MCP4752 (12 бит)")
+    
+        first_byte = self.wm | self.pds | number >> 8
+        second_byte = number & 0xFF
+        self.bus.write_byte_data(0x61, first_byte, second_byte)
+    
+        if self.verbose:
+            print(f"Число: {number}, отправленные данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
+
+    def set_voltage(self, voltage):
+        if(not(0.0 <= voltage <= self.dynamic_range)):
+            print(f"Voltage is out of range (0.0 ... {self.dynamic_range:.2f} V)")
+            print("Set zero.")
+            dac.set_number(0)
+        else: dac.set_number(int(voltage / self.dynamic_range * 255))
+
